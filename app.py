9044444fc82f10d3b31e23f13f5a4f8ffde0d40b
Sergei Novikov <trupewate@gmail.com>
2024-04-24 17:46:18 -0400

    minor change

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'kronoserj1418'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'drone_dispatch'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 mysql.init_app(app)
